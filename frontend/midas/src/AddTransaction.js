aca103276bb2ef7bfc85f0df29de075aa307aa3f
yihong <yihongliu00@gmail.com>
2020-07-19 12:27:55 -0400

    testing

--- frontend/midas/src/AddTransaction.js
+++ frontend/midas/src/AddTransaction.js
@@ -3,6 +3,12 @@ import React, { useState, useEffect } from 'react';
 import Cookies from 'js-cookie';
 
 export default function AddTransaction() {
+    const [unitPrice, setUnitPrice] = useState(0)
+    const [quant, setQuant] = useState(0)
+    const [total, setTotal] = useState(0)
+    useEffect(() => {
+        setTotal(unitPrice * quant)
+    })
 
     return (
         <div class="page-margins">
@@ -11,25 +17,25 @@ export default function AddTransaction() {
                     Unit Price
                 </div>
                 <div>
-                    <input type="text"></input>
+                    <input type="number"></input>
                 </div>
                 <div>
                     Quantity
                 </div>
                 <div>
-                    <input type="text"></input>
+                    <input type="number"></input>
                 </div>
                 <div>
                     Total value
                 </div>
                 <div>
-                    <input type="text"></input>
+                    <input type="number" value={total} onChange={(e)=>{setTotal(e.target.value)}}></input>
                 </div>
                 <div>
                     Category:
                 </div>
                 <div>
-                    <select id="category" name="category" onChange={()=>{}}>
+                    <select id="category" name="category" onChange={() => { }}>
                         <option value="" disabled selected>Select category</option>
                         <option value="Food">Food</option>
                         <option value="Toiletries">Toiletries</option>
