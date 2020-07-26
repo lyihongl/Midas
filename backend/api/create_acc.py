6859e8423316f142bd8f1cd8aeef6b122984f1a2
yihong <yihongliu00@gmail.com>
2020-07-25 21:37:39 -0400

    clean up

--- backend/api/create_acc.py
+++ backend/api/create_acc.py
@@ -2,15 +2,6 @@
 import bcrypt
 import data.users_model as users_model
 
-def CreateAcc(u, e, p, sql_inst):
-    #print(type(p))
+def CreateAcc(u, e, p):
     hashed = bcrypt.hashpw(p.encode('utf-8'), bcrypt.gensalt())
     users_model.InsertUser(u, e, hashed)
-    #query = "insert into users (username, email, password) values (%s, %s, %s)"
-    #cursor = sql_inst.GetCursor()
-
-    #print(type(hashed))
-    #print("len", len(hashed))
-    #cursor.execute(query, (u, e, hashed))
-    #cursor.commit()
-    #sql_inst.conn.commit()
--- backend/api/login.py
+++ backend/api/login.py
@@ -5,24 +5,14 @@ import data.users_model as users_model
 from flask import make_response
 
 
-def Login(u, p, sql_inst):
+def Login(u, p):
     usr = u[0]
 
     (userid, username, password)= users_model.SelectUser(["id", "username", "password"], ["username"], [usr])[0]
-    #print("result", result)
-    #print(query)
 
-    #cursor = sql_inst.GetCursor()
-    #query = "select * from users where username=%s;"
-    
-    #cursor.execute(query, [usr])
-
-    #for (Id, username, email, password) in cursor:
     if (password == bcrypt.hashpw(p[0].encode('utf-8'), password.encode('utf-8')).decode('utf-8')):
-    #        #pass
-        token = jwt.encode({'username':username, 'userid':userid}, 'secret', algorithm='HS256')
-        #response = {'token': 'valid'}
 
+        token = jwt.encode({'username':username, 'userid':userid}, 'secret', algorithm='HS256')
         response = make_response({'ok':'ok'})
         response.set_cookie('token', token)
         return response
